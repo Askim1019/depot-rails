0b5f2e39ab89a88ae4db6c469fb9a38a69d74bb5
Alex Kim <akim@futurefinance.com>
2018-09-06 13:46:03 -0500

    Depot Scaffold

--- db/migrate/20180905182301_create_products.rb
+++ db/migrate/20180905182301_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.string :ijmage_url
+      t.string :image_url
       t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,28 @@ Product.create(
   description:
     %{<p>
       <em>Native Apps, Multiple Platforms</em>
-      Answer the question "Can we build this for ALL the devices?" with a resounding YES. This book will help you get there with a real-worldintroduction to seven platforms, whether you're new to mobile or an experienced developer needing to expand your options. Plus, you'll find out which cross-platform solution makes the most sense for your needs.
+      Answer the question "Can we build this for ALL the devices?" with a resounding YES. This book will help yo u get there with a real-worldintroduction to seven platforms, whether you're new to mobile or an experienced developer needing to expand your options. Plus, you'll find out which cross-platform solution makes the most sense for your needs.
       </p>},
   image_url: '7apps.jpg',
   price: 26.00
   )
+
+Product.create(
+  title: 'Rails, Angular, Postgres, and Bootstrap',
+  description:
+    %{<p>
+      <em>Powerful, Effective, and Efficient</em> Full-Stack Web Development Textbook to help guide beginners to setup rails app.
+      </p>},
+  image_url: 'dcbang.jpg',
+  price: 35.00
+)
+
+Product.create(
+  title: 'Ruby Performance Optimization',
+  description:
+    %{<p>
+      Why Ruby Is Slow, and How to Fix It. You don't have to accept slow performance issues and slow processing in your web development environment. Learn how with this book.
+      </p>},
+  image_url: 'adrpo.jpg',
+  price: 30.00
+)
